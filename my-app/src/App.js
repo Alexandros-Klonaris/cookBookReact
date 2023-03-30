822d7372e8606ddd5bb5e019b6e5414155b4f5f4
prdj <pedrojr_dantas@hotmail.com>
2023-03-30 16:47:38 +0200

    router problems to solve

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import React from 'react';
+import Root from './components/Root';
+import HomePage from './components/HomePage';
+import PageOne from './components/pages/PageOne';
+import PageTwo from './components/pages/PageTwo';
+import PageThree from './components/pages/PageThree';
+
+import { useEffect, useState } from "react";
+import useContentful from "./components/useContentful";
+
+
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import RowJumbotron from './components/RowJumbotron';
 
+const router = createBrowserRouter( createRoutesFromElements (
+  /* Wrap this Root Route to create Router here */
+  <Route path="/" element={ <Root/> }>
+    <Route index element= {<HomePage />} />
+ 
+  </Route>))
+
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const { getPhotos } = useContentful();
+
+  console.log(photos)
+
+  useEffect(() => {
+    getPhotos().then((response) => response && setPhotos(response));
+  });
+
   return (
     <div className="App">
-     <Nav/>
-     <RowJumbotron/>
-     <Footer/>
+       
+    <RouterProvider router={ router }/>
     </div>
   );
 }
--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link, Outlet, Navigate } from "react-router-dom";
+
 
 export default function Nav() {
     return (
         
         <nav className="navbar-light navbar navbar-expand-sm">
-          <a className="navbar-brand" href="./index.html">
+          <Link className="navbar-brand" to="./index.html">
             <img src="./sources/brand-logo.jpeg" alt="Smoothie Logo" height="70"/>
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -15,25 +17,25 @@ export default function Nav() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav">
               <li className="nav-item active">
-                <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false"><span
-                    className="sr-only">(current)</span>Smoothies</a>
+                <Link className="nav-link dropdown-toggle" to="#" data-bs-toggle="dropdown" aria-expanded="false"><span
+                    className="sr-only">(current)</span>Smoothies</Link>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="./receipe_1.html">Green Smoothie</a></li>
+                  <li><Link className="dropdown-item" to="./receipe_1.html">Green Smoothie</Link></li>
                   <li>
                     <hr className="dropdown-divider"/>
                   </li>
-                  <li><a className="dropdown-item" href="./receipe_2.html">Strawberry Smoothie</a></li>
+                  <li><Link className="dropdown-item" to="./receipe_2.html">Strawberry Smoothie</Link></li>
                   <li>
                     <hr className="dropdown-divider"/>
                   </li>
-                  <li><a className="dropdown-item" href="./receipe_3.html">Banana Smoothie</a></li>
+                  <li><Link className="dropdown-item" to="./receipe_3.html">Banana Smoothie</Link></li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Health</a>
+                <Link className="nav-link" to="#">Health</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#" id="aboutUs">About us</a>
+                <Link className="nav-link" to="#" id="aboutUs">About us</Link>
               </li>
             </ul>
           </div>
